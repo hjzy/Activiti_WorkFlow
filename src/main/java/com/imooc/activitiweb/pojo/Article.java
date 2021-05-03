a78de3a0ba50e5f5e1bbb569c4b7efc8cbdbdbee
yifan <2540584259@qq.com>
2021-05-03 23:04:02 +0800

    完成公告添加

--- src/main/java/com/imooc/activitiweb/pojo/Article.java
+++ src/main/java/com/imooc/activitiweb/pojo/Article.java
@@ -15,48 +15,6 @@ public class Article {
 
     private int type;
 
-    private String ArticleViewUrl;
-
-    private String ArticleDeleteUrl;
-
-    private String ArticleEditUrl;
-
-    private String TypeName;
-    public String getTypeName()
-    {
-
-        switch (type)
-        {
-            case 0:
-                TypeName="测试用文章";
-                break;
-            case 1:
-                TypeName="壮学研究";
-                break;
-            case 2:
-                TypeName="壮乡见闻";
-                break;
-            case 3:
-                TypeName="文化艺术";
-                break;
-            case 4:
-                TypeName="历史印记";
-                break;
-            case 5:
-                TypeName="民风民俗";
-                break;
-            case 6:
-                TypeName="壮学文档";
-                break;
-            case 7:
-                TypeName="民族政策";
-                break;
-            default:
-                throw new IllegalStateException("Unexpected value: " + type);
-        }
-
-        return TypeName;
-    }
 
     public int getId() {
         return id;
@@ -91,19 +49,6 @@ public class Article {
     }
 
 
-    public String getArticleViewUrl() {
-        ArticleViewUrl = "/article/view/" + id;
-        return ArticleViewUrl;
-    }
 
-    public String getArticleDeleteUrl() {
-        ArticleDeleteUrl = "/article/delete/" + id;
-        return ArticleDeleteUrl;
-    }
-
-    public String getArticleEditUrl() {
-        ArticleEditUrl = "/article/toEdit/" + id;
-        return ArticleEditUrl;
-    }
 
 }
